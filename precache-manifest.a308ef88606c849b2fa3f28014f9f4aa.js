60271cf08e466261156df504cdf26d60ed1713c9
luis <luis.augusto.moggi@gmail.com>
2020-09-25 22:44:48 -0300

    ddelivery host

---
+++ precache-manifest.a308ef88606c849b2fa3f28014f9f4aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42595b0b5222a3fc0b7189a64dd878ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7335a558991ac4dec9c3",
+    "url": "/static/css/main.4cd65784.chunk.css"
+  },
+  {
+    "revision": "6358cfe23da5203efe84",
+    "url": "/static/js/2.f0fbabf3.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.f0fbabf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7335a558991ac4dec9c3",
+    "url": "/static/js/main.53b1935a.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  }
+]);
